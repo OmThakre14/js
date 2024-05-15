357cb9ff386e7eb2d8c39f8f0de37de11ab80a78
OmThakre14 <90558304+OmThakre14@users.noreply.github.com>
2024-05-15 15:01:50 +0530

    Create calc.js

---
+++ calc.js
@@ -0,0 +1,15 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+console.log(add(3,2));
+console.log(subtract(5,2));
+console.log(multiply(8,2));
